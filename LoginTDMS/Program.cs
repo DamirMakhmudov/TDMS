a8396a138b3410739bdd1f169f3c63135734dca8
Damir Makhmudov <samrukov@gmail.com>
2019-10-01 11:05:24 +0300

    renamed namespace

--- LoginTDMS/Program.cs
+++ LoginTDMS/Program.cs
@@ -3,13 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TDMS;
 
-namespace LoginTDMS
+namespace TDMSforBRIO
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+            TDMSApplication myapp = new TDMSApplication();
+
+            if (!myapp.IsLoggedIn) {
+                myapp.Login("sysadmin", "", "kosmos", @"192.168.16.8\tdmsserver");
+            }
+
+
+
+            Console.WriteLine(myapp.CurrentUser.Description);
+            Console.ReadKey();
+            myapp.Quit();
         }
     }
 }
