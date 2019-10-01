9e2d5bddd0e29f1cbd1554e03c359f556c8ba373
Damir Makhmudov <samrukov@gmail.com>
2019-10-01 11:43:06 +0300

    Добавлен блок обращения к Томам и файлам внутри

--- LoginTDMS/Program.cs
+++ LoginTDMS/Program.cs
@@ -11,16 +11,33 @@ namespace TDMSforBRIO
     {
         static void Main(string[] args)
         {
-
+            #region create tdms application and login 
             TDMSApplication myapp = new TDMSApplication();
 
             if (!myapp.IsLoggedIn) {
                 myapp.Login("sysadmin", "", "kosmos", @"192.168.16.8\tdmsserver");
             }
 
+            #endregion
 
+            #region get object_tom and files
 
-            Console.WriteLine(myapp.CurrentUser.Description);
+            //подключаемся к объекту по GUID
+            string GUID = "{49DFA37A-E760-4C83-A084-9C270A47DA39}";
+            TDMSObject buildingObject = myapp.GetObjectByGUID(GUID);
+
+            // получаем все Томы (object_tom) данного Объекта (object_object). Чертежи хранятся в файловом составе Томов
+            // лучше создать ручную выборку для получения Томов в примере получение черерз метод ContentAll - сквозной состав объекта
+
+            TDMSObjects toms = buildingObject.ContentAll.ObjectsByDef("OBJECT_TOM"); //метод ObjectsByDef позволяет во всей коллекции объектов осуществить фильтр по типу объекта
+
+            TDMSObject firstTom = toms[0]; 
+            TDMSFiles tomsFiles = firstTom.Files;
+            TDMSFile firstFile = tomsFiles[0];
+
+            firstFile.CheckOut(firstFile.WorkFileName);
+            #endregion
+
             Console.ReadKey();
             myapp.Quit();
         }
